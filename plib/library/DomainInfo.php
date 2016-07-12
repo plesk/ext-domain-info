1d0563dec7070bb40430178a70672b032d82324c
Igor Antipkin <iantipkin@plesk.com>
2016-07-12 17:39:03 +0600

    It is possible to get limits and permissions added by extension. Remove get Plesk limits and permissions methods

--- plib/library/DomainInfo.php
+++ plib/library/DomainInfo.php
@@ -17,29 +17,6 @@ class Modules_DomainInfo_DomainInfo
             'clientLogin' => $domain->getClient()->getProperty('login'),
             'hasHosting' => pm_Locale::lmsg($domain->hasHosting() ? 'yes' : 'no'),
             'ipAddresses' => $domain->getIpAddresses(),
-            'limits' => [
-                'max_site' => static::getLimit($domain, 'max_site'),
-            ],
-            'permissions' => [
-                'manage_dns' => static::getPermission($domain, 'manage_dns'),
-            ]
         ];
-    }
-
-    protected static function getLimit(pm_Domain $domain, $limitName)
-    {
-        $limit = $domain->getLimit($limitName);
-
-        return -1 === $limit ? pm_Locale::lmsg('unlimited') : $limit;
-    }
-
-    protected static function getPermission(pm_Domain $domain, $permissionName)
-    {
-        $permission = $domain->hasPermission($permissionName);
-        if (is_null($permission)) {
-            return pm_Locale::lmsg('undefined');
-        }
-
-        return pm_Locale::lmsg($permission ? 'yes' : 'no');
     }
 }
